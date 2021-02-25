0d8bdf4b8448c31d0fa922005ed34fb7f30aedf8
Deadmau5-droid <72690130+Deadmau5-droid@users.noreply.github.com>
2021-02-26 01:43:45 +0530

    s

---
+++ School.dataService/Migrations/20210225190812_intial2.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace School.dataService.Migrations
+{
+    public partial class intial2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
